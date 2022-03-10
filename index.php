584a8ae9e95937c0b9b50c72f17ad47a1307f95e
HelloItsDella <100497212+HelloItsDella@users.noreply.github.com>
2022-03-10 12:57:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <meta property="og:image" content="shibelogo.png">
     <meta property="og:title" content="Della's Website!">
     <meta property="og:url" content="https://helloitsdella.herokuapp.com/">
